673a2d786f8ffab56ff4401b0d6e88a3a826fd1d
Love <lovevashista01@gmail.com>
2026-06-05 23:16:58 +0530

    login.page.js - modification

--- test/pages/login.page.js
+++ test/pages/login.page.js
@@ -26,8 +26,8 @@
  */
 
 import BasePage from './base.page.js';
-import { selectFor} from '../utils/platform.js';
-import {hideKeyboard } from '../utils/gestures.js';
+import { selectFor } from '../utils/platform.js';
+import { hideKeyboard } from '../utils/gestures.js';
 import logger from '../utils/logger.js';
 
 class LoginPage extends BasePage {
